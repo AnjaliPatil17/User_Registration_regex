0e22e198403d76decf99dd08502b6150e2d26097
Anjali <anjaliap1227@gmail.com>
2024-06-11 12:32:41 +0530

    Welcome to User registration regex

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Main {
+    public static void firstName(String name){
+        Pattern pattern=Pattern.compile("[A-Z][a-z]{3,}");
+        Matcher match= pattern.matcher(name);
+        boolean ismatch= match.matches();
+        if(ismatch){
+            System.out.println("Valid input");
+        }else {
+            System.out.println("Inavaid input");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the first name: ");
+        String name= sc.nextLine();
+        firstName(name);
+    }
+}
