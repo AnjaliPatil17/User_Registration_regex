158830ebc23589b41077388ba7c7441c94b64d7f
Anjali <anjaliap1227@gmail.com>
2024-06-11 15:11:12 +0530

    UC2-LastName_Validation file add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,12 +15,26 @@ public class Main {
         }
     }
 
+    public static void lastName(String lastname){
+        Pattern pattern=Pattern.compile("[A-Z][a-z]{3,}");
+        Matcher match= pattern.matcher(lastname);
+        boolean ismatch= match.matches();
+        if(ismatch){
+            System.out.println("Valid input");
+        }else {
+            System.out.println("Inavaid input");
+        }
+    }
+
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the first name: ");
         String firstname= sc.nextLine();
         firstName(firstname);
-     
+        System.out.print("Enter the Last name: ");
+        String lastname=sc.nextLine();
+        lastName(lastname);
+
     }
 }
