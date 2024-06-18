163af0473420534ea8d9af535f294bc4ce7be008
Anjali <anjaliap1227@gmail.com>
2024-06-18 21:32:05 +0530

    Welcome to User Registeration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,68 +15,12 @@ public class Main {
         }
     }
 
-    public static void lastName(String lastname){
-        Pattern pattern=Pattern.compile("[A-Z][a-z]{3,}");
-        Matcher match= pattern.matcher(lastname);
-        boolean ismatch= match.matches();
-        if(ismatch){
-            System.out.println("Valid input");
-        }else {
-            System.out.println("Inavaid input");
-        }
-    }
-
-    public static void emailId(String emailid){
-        Pattern pattern=Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
-        Matcher match= pattern.matcher(emailid);
-        boolean ismatch= match.matches();
-        if(ismatch){
-            System.out.println("Valid input");
-        }else {
-            System.out.println("Inavaid input");
-        }
-    }
-
-    public static void phoneNo(String phoneno){
-        Pattern pattern=Pattern.compile("^[0-9]{2} [0-9]{10}$");
-        Matcher match= pattern.matcher(phoneno);
-        boolean ismatch= match.matches();
-        if(ismatch){
-            System.out.println("Valid input");
-        }else {
-            System.out.println("Invaid input");
-        }
-    }
-
-    public static void Password(String password){
-        Pattern pattern=Pattern.compile("^[a-z]{8}$");
-        Matcher match= pattern.matcher(password);
-        boolean ismatch= match.matches();
-        if(ismatch){
-            System.out.println("Valid input");
-        }else {
-            System.out.println("Invaid input");
-        }
-    }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the first name: ");
         String firstname= sc.nextLine();
         firstName(firstname);
-        System.out.print("Enter the Last name: ");
-        String lastname=sc.nextLine();
-        lastName(lastname);
-        System.out.print("Enter the Email ID : ");
-        String emailid= sc.nextLine();
-        emailId(emailid);
-        System.out.print("Enter the Phone no : ");
-        String phoneno= sc.nextLine();
-        phoneNo(phoneno);
-        System.out.print("Enter the Password : ");
-        String password= sc.nextLine();
-        Password(password);
+
     }
 }
-
-
